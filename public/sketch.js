22b8c00954d5e88ebb976c44fde5a6ea5106d680
Miosekai <1010070539@u.icesi.edu.co>
2022-08-17 23:09:32 -0500

    primer pokemon

--- public/sketch.js
+++ public/sketch.js
@@ -1,40 +1,29 @@
 let canvas;
-let pokemonB = '"https://pokeapi.co/api/v2/pokemon/1/"},';
+let SPRITE_PATH_URL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/';
 let bulbasaur = null;
-let img;
-let pokemon1 = null;
+let charmander = null;
+let squirtle = null;
 
 function setup() {
     frameRate(60);
-    canvas = createCanvas(windowWidth, windowHeight);
-
 }
 
 function draw() {
     //background(0, 50);
     background(0);
+    newCursor();
 
-
+    if(bulbasaur != null){
+        fill(255);
+        textSize(24);
+        text(bulbasaur.name, 140, 100);
+        image(bulbasaur.pImage, 50, 100, 100, 100);
+    }
+    
 }
 
-    async function poke1(){
-        const URL =  'https://pokeapi.co/api/v2/pokemon?limit=9&offset=0';
-        const query = await fetch(URL);
-        const data = await query.json();
-        const {results} = data;
-        console.log(data);
-        console.log(results);
-        console.log()
-        
-       
-    
-    
-    
-    }
-        
-
 function mouseClicked(){
-    poke1();
+    getPokemonList()
 }
 
 function windowResized() {
@@ -45,4 +34,21 @@ function newCursor() {
     noStroke();
     fill(255);
     ellipse(pmouseX, pmouseY, 10, 10);
+}
+
+async function getPokemonList(){
+    const POKEAPI_LIST_URL = 'https://pokeapi.co/api/v2/pokemon?limit=10&offset=0'
+    const query = await fetch(POKEAPI_LIST_URL);
+    const data = await query.json();
+    const { results } = data;
+
+    let pokemon = results[0];
+    let temporaryArray = pokemon.url.split('/');
+    pokemon.sprite =  SPRITE_PATH_URL + temporaryArray[6] + '.png';
+    loadImage(pokemon.sprite, image => {
+        pokemon.pImage = image;
+        console.log(pokemon);
+        bulbasaur = pokemon;
+    });
+
 }
